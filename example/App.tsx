83bffa4c9a5e0cad8129012722ca44011ebefc05
Moritz Moeller <mm@mxs.de>
2019-09-01 21:44:35 +0200

    ..

--- example/App.tsx
+++ example/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { Linking } from 'react-native';
 
 const AppNavigator = createStackNavigator({
   Menu: {
@@ -26,6 +27,15 @@ const AppNavigator = createStackNavigator({
 const AppContainer = createAppContainer(AppNavigator);
 
 class App extends React.PureComponent<{}> {
+  public componentDidMount() {
+    Linking.addEventListener('url', ({url}) => {
+      console.log('linking url', url);
+    });
+    Linking.getInitialURL().then((url) => {
+      if (!url) return;
+      console.log('linking url', url);
+    });
+  }
   public render() {
     return (
       <AppContainer />
