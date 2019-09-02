d66b77a8619ba350ba8bbc6baca79d7ca47a4b0e
Moritz Moeller <mm@mxs.de>
2019-09-02 20:37:07 +0200

    ..

--- example/App.tsx
+++ example/App.tsx
@@ -29,12 +29,13 @@ const AppContainer = createAppContainer(AppNavigator);
 class App extends React.PureComponent<{}> {
   public componentDidMount() {
     Linking.addEventListener('url', ({url}) => {
-      console.log('linking url', url);
+      console.log('linking url event', url);
     });
     Linking.getInitialURL().then((url) => {
+      console.log('linking url initial', url);
       if (!url) return;
-      console.log('linking url', url);
     });
+    // 2019-09-02 20:32:06.976926+0200 example[862:485610] 'linking url', 'file:///private/var/mobile/Containers/Data/Application/14601C07-5EAB-4428-B111-B2B8AC716736/tmp/org.reactjs.native.example.example-Inbox/2018_07_24_12_51_03.pdf'
   }
   public render() {
     return (
