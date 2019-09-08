f0cc4ac7b90e7ecb7ad1e2d73839a911ac2873f6
Moritz Moeller <mm@mxs.de>
2019-09-08 17:33:26 +0200

    ..

--- example/App.tsx
+++ example/App.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import { Linking } from 'react-native';
 import { Fs } from 'react-native-mo-fs';
 
 const AppNavigator = createStackNavigator({
@@ -29,18 +28,30 @@ const AppContainer = createAppContainer(AppNavigator);
 
 
 if (Fs.ios.Events) {
-  Fs.ios.Events.addListener('ReactNativeMoFsOpenURL', (asd) => {
+  Fs.ios.Events.addListener('ReactNativeMoFsOpenURL', async (asd) => {
     console.log('XXX ReactNativeMoFsOpenURL', asd);
+    const blob = await Fs.readURL(asd.url);
+    console.log('blob', blob);
+    blob.close();
     // 'XXX ReactNativeMoFsOpenURL', { url: 'file:///private/var/mobile/Containers/Data/Application/8250D055-04FF-483C-9861-F5E3BF611DE3/tmp/org.reactjs.native.example.example-Inbox/2018_07_24_12_51_03.pdf',
     //   options: { UIApplicationOpenURLOptionsOpenInPlaceKey: false } }
   });
-  Fs.ios.Module!.getLastOpenURL().then((asd) => {
+  Fs.ios.Module!.getLastOpenURL().then(async (asd) => {
     console.log('XXX getLastOpenURL', asd);
+    if (!asd) return;
+    const blob = await Fs.readURL(asd.url);
+    console.log('blob', blob);
+    blob.close();
   });
 }
 if (Fs.android.Events) {
-  Fs.android.Events.addListener('ReactNativeMoFsNewIntent', (asd) => {
+  Fs.android.Events.addListener('ReactNativeMoFsNewIntent', async (asd) => {
     console.log('XXX ReactNativeMoFsNewIntent', asd);
+    if (asd.action === 'android.intent.action.SEND' && asd.extras && asd.extras['android.intent.extra.STREAM']) {
+      const blob = await Fs.readURL(asd.extras['android.intent.extra.STREAM']);
+      console.log('blob', blob);
+      blob.close();
+    }
     // 'XXX ReactNativeMoFsLink', { extras:
     //    { 'android.intent.extra.TITLE': 'sample.pdf',
     //      'android.intent.extra.STREAM': 'content://com.android.providers.downloads.documents/document/695',
@@ -50,8 +61,13 @@ if (Fs.android.Events) {
     //   type: 'application/pdf',
     //   action: 'android.intent.action.SEND' }
   });
-  Fs.android.Module!.getInitialIntent().then((asd) => {
+  Fs.android.Module!.getInitialIntent().then(async (asd) => {
     console.log('XXX getInitialIntent', asd);
+    if (asd.action === 'android.intent.action.SEND' && asd.extras && asd.extras['android.intent.extra.STREAM']) {
+      const blob = await Fs.readURL(asd.extras['android.intent.extra.STREAM']);
+      console.log('blob', blob);
+      blob.close();
+    }
     // 'XXX getInitialIntent', { extras:
     //        { 'android.intent.extra.TITLE': 'sample.pdf',
     //          'android.intent.extra.STREAM': 'content://com.android.providers.downloads.documents/document/695',
@@ -62,13 +78,6 @@ if (Fs.android.Events) {
     //       action: 'android.intent.action.SEND' }
   });
 }
-Linking.addEventListener('url', ({url}) => {
-  console.log('XXX linking url event', url);
-});
-Linking.getInitialURL().then((url) => {
-  console.log('XXX linking url initial', url);
-  if (!url) return;
-});
 
 class App extends React.PureComponent<{}> {
   public render() {
