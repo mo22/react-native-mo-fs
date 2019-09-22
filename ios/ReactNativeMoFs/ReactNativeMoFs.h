27fadac028ac84cd1e22e5522b41f76cab0b899b
Moritz Moeller <mm@mxs.de>
2019-09-22 17:48:11 +0200

    ..

--- ios/ReactNativeMoFs/ReactNativeMoFs.h
+++ ios/ReactNativeMoFs/ReactNativeMoFs.h
@@ -3,6 +3,9 @@
 
 @interface ReactNativeMoFs : RCTEventEmitter
 
++ (bool)verbose;
++ (void)setVerbose:(BOOL)verbose;
+
 + (void)disableAutoSwizzle;
 + (BOOL)application:(UIApplication *)application openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options;
 
