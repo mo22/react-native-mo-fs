b8ec97cf54639fe5702b8da6e16f8f62ac4000e2
Moritz Moeller <mm@mxs.de>
2019-09-22 17:47:40 +0200

    ..

--- ios/ReactNativeMoFs/ReactNativeMoFs.h
+++ ios/ReactNativeMoFs/ReactNativeMoFs.h
@@ -2,4 +2,8 @@
 #import <React/RCTEventEmitter.h>
 
 @interface ReactNativeMoFs : RCTEventEmitter
+
++ (void)disableAutoSwizzle;
++ (BOOL)application:(UIApplication *)application openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options;
+
 @end
