bfdfb3d90b6999663b3704017faebcd885a494d9
Moritz Moeller <mm@mxs.de>
2019-11-28 19:45:45 +0100

    ..

--- example/Menu.tsx
+++ example/Menu.tsx
@@ -220,20 +220,26 @@ export default class Menu extends React.Component<NavigationInjectedProps> {
               {
                 const key = Buffer.from('01234567890123456789012345678901');
                 const iv = Buffer.from('01234567890123456789012345678901');
+                
                 const cipher = forge.cipher.createCipher('AES-CBC', new forge.util.ByteStringBuffer(key));
                 cipher.start({ iv: iv.toString('binary') });
                 cipher.update(new forge.util.ByteStringBuffer(data));
                 cipher.finish();
                 const cipherRef = Buffer.from(cipher.output.data, 'binary');
+                
                 const enc = await Fs.cryptBlob(
                   blob,
                   '',
                   'encrypt',
-                  key.toString('base64'),
-                  iv.toString('base64'),
+                  key,
+                  iv,
                 );
                 const encData = Buffer.from(await Fs.readBlob(enc, 'arraybuffer'));
                 enc.close();
+                // console.log('A', cipherRef.byteLength);
+                // console.log('B', encData.byteLength);
+                // console.log('A', cipherRef.toString('base64'));
+                // console.log('B', encData.toString('base64'));
                 if (cipherRef.toString('base64') !== encData.toString('base64')) throw new Error('aes failure');
               }
               Alert.alert('hashes match');
